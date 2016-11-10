fb3f4649acc1459bfa1f3a786332b6de583f2d82
Jordan Lui <jordan.lui@gmail.com>
2016-11-10 11:44:44 -0800

    Updates to Overfitting Demo

---
+++ Overfitting.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 10:35:29 2016
+
+@author: Jordan
+"""
+
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import cross_val_score # works for older
+#from sklearn.model_selection import cross_val_score #Newer sklearn might work with this
+
+np.random.seed(0)
+
+n_samples = 30
+degrees = [1, 4, 15]
+
+true_fun = lambda X: np.cos(1.5 * np.pi * X)
+X = np.sort(np.random.rand(n_samples))
+y = true_fun(X) + np.random.randn(n_samples) * 0.1
+
+plt.figure(figsize=(14, 5))
+for i in range(len(degrees)):
+    ax = plt.subplot(1, len(degrees), i + 1)
+    plt.setp(ax, xticks=(), yticks=())
+
+    polynomial_features = PolynomialFeatures(degree=degrees[i],
+                                             include_bias=False)
+    linear_regression = LinearRegression()
+    pipeline = Pipeline([("polynomial_features", polynomial_features),
+                         ("linear_regression", linear_regression)])
+    pipeline.fit(X[:, np.newaxis], y)
+
+    # Evaluate the models using crossvalidation
+#    scores = cross_val_score(pipeline, X[:, np.newaxis], y,
+#                             scoring="neg_mean_squared_error", cv=10)
+    scores = cross_val_score(pipeline, X[:, np.newaxis], y,
+                             scoring="mean_squared_error", cv=10)
+
+    X_test = np.linspace(0, 1, 100)
+    plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
+    plt.plot(X_test, true_fun(X_test), label="True function")
+    plt.scatter(X, y, label="Samples")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.xlim((0, 1))
+    plt.ylim((-2, 2))
+    plt.legend(loc="best")
+    plt.title("Degree {}\nMSE = {:.2e}(+/- {:.2e})".format(
+        degrees[i], -scores.mean(), scores.std()))
+plt.show()
